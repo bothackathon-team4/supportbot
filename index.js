c76b59cedcb3a7c92b4cf177337511a9cca79312
Stefan Hojer <mail@stefanhojer.de>
2017-02-21 16:14:01 +0100

    text2speech

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var builder = require('botbuilder');
+//var builder = require('botbuilder');
 var calling = require('botbuilder-calling');
 var restify = require('restify');
+var speechService = require('./bingspeech.js');
+var streamify = require('streamify');
 
 //=========================================================
 // Bot Setup
@@ -13,10 +15,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 // Create chat bot
-var chatConnector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
+//var chatConnector = new builder.ChatConnector({
+//    appId: process.env.MICROSOFT_APP_ID,
+//    appPassword: process.env.MICROSOFT_APP_PASSWORD
+//});
 var callConnector = new calling.CallConnector({
     callbackUrl: 'https://da0b3a6e.ngrok.io/api/calls',
     appId: process.env.MICROSOFT_APP_ID,
@@ -24,17 +26,17 @@ var callConnector = new calling.CallConnector({
 });
 
 var callingBot = new calling.UniversalCallBot(callConnector);
-var chatBot = new builder.UniversalBot(chatConnector);
-server.post('/api/messages', chatConnector.listen());
+//var chatBot = new builder.UniversalBot(chatConnector);
+//server.post('/api/messages', chatConnector.listen());
 server.post('/api/calls', callConnector.listen());
 
 //=========================================================
 // Chat Dialogs
 //=========================================================
 
-chatBot.dialog('/', function (session) {
-   session.send('Chat...');
-});
+//chatBot.dialog('/', function (session) {
+//   session.send('Chat...');
+//});
 
 //=========================================================
 // Calling Dialogs
@@ -43,29 +45,34 @@ chatBot.dialog('/', function (session) {
 // Add root dialog
 callingBot.dialog('/', [
   function (session) {
-      calling.Prompts.choice(session, "Which department? Press 1 for support, 2 for billing, 3 for claims, or star to return to previous menu.", [
-          { name: 'support', dtmfVariation: '1', speechVariation: ['support', 'customer service'] },
-          { name: 'billing', dtmfVariation: '2', speechVariation: ['billing'] },
-          { name: 'claims', dtmfVariation: '3', speechVariation: ['claims'] },
-          { name: '(back)', dtmfVariation: '*', speechVariation: ['back', 'previous'] }
-      ]);
+    calling.Prompts.record(session, "Please leave a message after the beep.", {
+      recordingFormat: 'wav',
+      playBeep: false,
+      maxSilenceTimeoutInSeconds: 3
+    });
   },
-  function (session, results) {
-      if (results.response !== '(back)') {
-          session.beginDialog('/' + results.response.entity + 'Menu');
-      } else {
-          session.endDialog();
+  function (session, result) {
+    //console.log(result.response);
+    //called...{ recordedAudio: <Buffer 52 ... >, lengthOfRecordingInSecs: 7.2459999999999996 }
+
+    console.log("got result");
+
+    // FIXME: convert buffer to stream in a sane way
+    var fs = require('fs');
+    fs.writeFile("/tmp/braindead.wav", result.response.recordedAudio, function(err) {
+      if(err) {
+        return console.log(err);
       }
-  },
-  function (session) {
-      // Loop menu
-      session.replaceDialog('/');
+    });
+    var stream = fs.createReadStream("/tmp/braindead.wav")
+
+    speechService.getTextFromAudioStream(stream).then(text => {
+      console.log(text);
+    }).catch(error => {
+      console.log(error);
+    })
   }
 ]);
-
-callingBot.dialog('/claimsMenu', function (session) {
-    session.send("You selected claims.");
-})
 
 /*
 var intents = new builder.IntentDialog();
