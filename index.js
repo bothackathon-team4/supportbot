f8edb7ec6692fe3bfe3b29543f77d254d7715c6b
Stefan Hojer <mail@stefanhojer.de>
2017-02-21 21:42:53 +0100

    fixed typo

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var chatConnector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var callConnector = new calling.CallConnector({
-    callbackUrl: process.env.CALLBACK_URL,,
+    callbackUrl: process.env.CALLBACK_URL,
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
